5971bda1902f15f48dd9dec308959bd0ce0fabc0
shahan12 <shahankhan485@gmail.com>
2024-12-22 15:20:36 +0530

    added search functionality in todos

--- components/header.tsx
+++ components/header.tsx
@@ -1,15 +1,17 @@
-"use client"
+"use client";
+import { useBoardStore } from "@/store/boardStore";
 import { MagnifyingGlassIcon, UserCircleIcon } from "@heroicons/react/16/solid";
 import Image from "next/image";
 import React from "react";
 import Avatar from "react-avatar";
 
 function Header() {
+  const searchString = useBoardStore((state) => state.searchString);
+  const setSearchString = useBoardStore((state) => state.setSearchString);
   return (
     <header className="">
       <div className="flex flex-col md:flex-row items-center p-5 bg-gray-500/10 rounded-b-2xl ">
-
-    <div className="absolute top-0 left-0 w-full h-96 bg-gradient-to-br from-pink-400 to-[#0055D1] rounded-md filter blur-3xl opacity-50 -z-50"></div>
+        <div className="absolute top-0 left-0 w-full h-96 bg-gradient-to-br from-pink-400 to-[#0055D1] rounded-md filter blur-3xl opacity-50 -z-50"></div>
         <Image
           src="https://links.papareact.com/c2cdd5"
           alt="Trello Logo"
@@ -21,17 +23,21 @@ function Header() {
           {/**Search Box */}
           <form className="flex items-center space-x-5 bg-white rounded-md p-2 shadow-md flex-1 md:flex-initial">
             <MagnifyingGlassIcon className="h-6 w-6 text-gray-400" />
-            <input type="text" className="flex-1 outline-none p-2" />
+            <input
+              type="text"
+              className="flex-1 outline-none p-2"
+              value={searchString}
+              onChange={(e) => setSearchString(e.target.value)}
+            />
             <button hidden={true}>Search</button>
           </form>
-          <Avatar name = 'Shahan Khan' round color="#0055D1" size="50" />
+          <Avatar name="Shahan Khan" round color="#0055D1" size="50" />
         </div>
       </div>
       <div className="flex items-center justify-center px-5 py-2 md:py-5">
-        
         <p className="flex items-center p-5 text-sm font-light pr-5  shadow-xl rounded-xl w-fit bg-white italic max-w-3xl text-[#0055D1]">
-            <UserCircleIcon className="inline-block  h-10 w-10 text-[#0055D1] mr-1"/>
-            GPT is working
+          <UserCircleIcon className="inline-block  h-10 w-10 text-[#0055D1] mr-1" />
+          GPT is working
         </p>
       </div>
     </header>
