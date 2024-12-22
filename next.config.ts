a4cd7d5f9988bb61f101f01ef19fe99b81911e63
shahan12 <shahankhan485@gmail.com>
2024-12-22 22:17:29 +0530

    disabled lints for production

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images :{
     domains :["links.papareact.com"]
-  }
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   /* config options here */
 };
 
