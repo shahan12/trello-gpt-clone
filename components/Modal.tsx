c32f5967ce87d70c0bc08b07cfb8eb6f8f0f2fff
shahan12 <shahankhan485@gmail.com>
2024-12-26 12:26:11 +0530

    Add new note feature popup added

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -2,6 +2,7 @@
 import { useModalStore } from "@/store/ModalStore";
 import {
   Checkbox,
+  Description,
   Dialog,
   DialogPanel,
   DialogTitle,
@@ -24,10 +25,23 @@ function Modal() {
       >
         <div className="fixed inset-0 flex w-screen items-center justify-center p-4">
           <DialogPanel className="max-w-lg space-y-4 border bg-white p-12">
-            <DialogTitle>Deactivate account order</DialogTitle>
+            <DialogTitle>Deactivate account</DialogTitle>
+            <Description>
+              This will permanently deactivate your account
+            </Description>
+            <p>
+              Are you sure you want to deactivate your account? All of your data
+              will be permanently removed.
+            </p>
 
-            {/* ... */}
+            {/*
+          You can render additional buttons to dismiss your
+          dialog by setting `isOpen` to `false`.
+        */}
+            <button onClick={closeModal}>Cancel</button>
+            <button>ADD</button>
           </DialogPanel>
+          {/* ... */}
         </div>
       </Dialog>
     </>
